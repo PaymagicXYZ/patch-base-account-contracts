97b13133ed05ca1dd6e09874b7294d7a87433a19
David Eiber <davideiber@gmail.com>
2023-02-13 17:31:16 -0500

    Add use of paymaster to SimpleUserOpTransaction test

--- test/SimpleUserOpTransaction.ts
+++ test/SimpleUserOpTransaction.ts
@@ -6,8 +6,8 @@ import {
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { parseEther } from "ethers/lib/utils";
-import { fillAndSign } from "../utils";
+import { parseEther, hexConcat, arrayify } from "ethers/lib/utils";
+import { fillAndSign, getBalance } from "../utils";
 
 let entryPoint: EntryPoint;
 let wallet: BaseAccount;
@@ -20,9 +20,16 @@ describe("Wallet", function () {
     const EntryPoint = await ethers.getContractFactory("EntryPoint");
     const entryPoint = await EntryPoint.deploy();
 
-    await owner.sendTransaction({
-      to: entryPoint.address,
-      value: ethers.utils.parseEther("2"),
+    const VerifyingPaymaster = await ethers.getContractFactory(
+      "VerifyingPaymaster"
+    );
+    const verifyingPaymaster = await VerifyingPaymaster.deploy(
+      entryPoint.address,
+      owner.address
+    );
+
+    await entryPoint.depositTo(verifyingPaymaster.address, {
+      value: parseEther("2"),
     });
 
     const BaseAccountFactory = await ethers.getContractFactory(
@@ -45,14 +52,13 @@ describe("Wallet", function () {
       baseAccountAddress
     );
 
-    await owner.sendTransaction({
-      to: baseAccountAddress,
-      value: ethers.utils.parseEther("2"),
-    });
-
     const TestToken = await ethers.getContractFactory("TestToken");
     const testToken = await TestToken.deploy(
       baseAccountContract.address,
+      parseEther("2.0")
+    );
+
+    expect(await testToken.balanceOf(baseAccountContract.address)).to.eq(
       parseEther("2.0")
     );
 
@@ -61,14 +67,21 @@ describe("Wallet", function () {
       beneficiary,
       testToken,
       entryPoint,
+      verifyingPaymaster,
       baseAccountContract,
     };
   }
 
   describe("Send tx", function () {
     it("Should correctly execute UserOp from Entrypoint", async function () {
-      const { owner, beneficiary, testToken, entryPoint, baseAccountContract } =
-        await loadFixture(deployWalletFixture);
+      const {
+        owner,
+        beneficiary,
+        verifyingPaymaster,
+        testToken,
+        entryPoint,
+        baseAccountContract,
+      } = await loadFixture(deployWalletFixture);
 
       const { data } = await testToken.populateTransaction.transfer(
         owner.address,
@@ -81,7 +94,7 @@ describe("Wallet", function () {
         data!
       );
 
-      const op = await fillAndSign(
+      let op = await fillAndSign(
         {
           sender: baseAccountContract.address,
           nonce: await baseAccountContract.nonce(),
@@ -91,7 +104,26 @@ describe("Wallet", function () {
         entryPoint
       );
 
+      const hash = await verifyingPaymaster.getHash(op);
+      const sig = await owner.signMessage(arrayify(hash));
+
+      op.paymasterAndData = hexConcat([verifyingPaymaster.address, sig]);
+
+      op = await fillAndSign(op, owner, entryPoint);
+
+      const baseAccountBalanceBefore = await getBalance(
+        baseAccountContract.address
+      );
+
+      expect(await getBalance(verifyingPaymaster.address)).to.eq(0);
+
       const tx = await entryPoint.handleOps([op], beneficiary.address);
+
+      const baseAccountBalanceAfter = await getBalance(
+        baseAccountContract.address
+      );
+
+      expect(baseAccountBalanceBefore).to.eq(baseAccountBalanceAfter);
 
       expect(await testToken.balanceOf(baseAccountContract.address)).to.eq(0);
     });
